cf7658aa715a77ca68761f4f041dc9ccbe69f097
jyj1143 <jyj1143@gmail.com>
2022-09-29 10:11:02 +0900

    Create Member domain Test

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //추상화(memberRepository)에도 의존하지만  구현체(MemoryMemberRepository)이도 의존하고 있다.
 
     @Override
     public void join(Member member) {
