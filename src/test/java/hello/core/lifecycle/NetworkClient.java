6c77568e41815e34cd8f89965f42ef9e4ed45ec5
jyj1143 <jyj1143@gmail.com>
2022-10-09 16:27:07 +0900

    Use PostConstruct, PreDestroy Annotation

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,8 @@
 package hello.core.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class NetworkClient {
 
@@ -29,13 +31,13 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
-
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnet();
     }
 
-
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connet();
